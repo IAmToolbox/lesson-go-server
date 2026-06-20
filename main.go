3e8a99ea5794a9cd1aecec86f4dc4dd8cc3e94c4
Toolbox <danielalejandro.leivafunes9@gmail.com>
2026-06-19 20:57:13 -0500

    Added some new handlers and method-specific logic

--- main.go
+++ main.go
@@ -5,12 +5,35 @@ import (
 	"net/http"
 	"os"
 	"log"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Swap(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("Hits Reset"))
+	if err != nil {
+		log.Fatalf("failed to write response: %w", err)
+	}
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+	var config apiConfig
+	mux.Handle("/app/", config.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))) // Look at all those closing parenthesis brooo
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte("OK"))
@@ -18,6 +41,15 @@ func main() {
 			log.Fatalf("failed to write response: %w", err)
 		}
 	})
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(fmt.Sprintf("Hits: %v", config.fileserverHits.Load())))
+		if err != nil {
+			log.Fatalf("failed to write response: %w", err)
+		}
+	})
+	mux.HandleFunc("POST /reset", config.metricsReset)
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
