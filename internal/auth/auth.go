085f84ddf58f8054395eb9c0e4a17148279092fd
Toolbox <danielalejandro.leivafunes9@gmail.com>
2026-06-28 21:30:33 -0500

    Brain is mush will continue later

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,12 @@
 package auth
 
 import (
+	"log"
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -16,4 +21,25 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 	match, err := argon2id.ComparePasswordAndHash(password, hash)
 	return match, err
 
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy-access",
+		IssuedAt: jwt.NewNumericDate(time.Now()),
+		ExpiresAt: expiresIn,
+		Subject: userID.String(),
+	})
+	tokenString, err := token.SignedString([]byte(tokenSecret))
+	return tokenString, err
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return uuid.Parse(token.Claims.Subject), nil
 }
